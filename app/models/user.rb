9e53c5300ac9e2e6424968ad16123edb574800b5
masa0824 <87467360+masa0824@users.noreply.github.com>
2022-01-26 12:29:08 +0900

    アカウント登録のバリデーションとエラーCSSの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ApplicationRecord
     has_secure_password
+
+    #バリデーションの定義
+    MaxLength1 = 255
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+    validates :email,
+        presence: true,
+        length: { maximum: MaxLength1 },
+        format: { with: VALID_EMAIL_REGEX }
+
+    #validates :password,
+    #    presence: true
 end

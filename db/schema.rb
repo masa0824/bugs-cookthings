f76b0e2f82bf3d022656b8a8c8be653886b16949
masa0824 <87467360+masa0824@users.noreply.github.com>
2022-01-31 18:26:02 +0900

    パスワードリマインダー[カラム追加]
    ※実行コマンド
    rails g migration add_reset_to_users reset_digest:string reset_sent_at:datetime
    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_08_150742) do
+ActiveRecord::Schema.define(version: 2022_01_31_065755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 2020_09_08_150742) do
     t.string "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 

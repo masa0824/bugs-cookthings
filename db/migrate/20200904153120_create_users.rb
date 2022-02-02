997cce84eebc9b892e84690b3ea5e9b45ea72641
masa0824 <87467360+masa0824@users.noreply.github.com>
2022-02-02 11:58:07 +0900

    DBカラムの順番変更

--- db/migrate/20200904153120_create_users.rb
+++ db/migrate/20200904153120_create_users.rb
@@ -6,6 +6,13 @@ class CreateUsers < ActiveRecord::Migration[6.0]
       t.string :email, :null => false, limit: 255
       t.string :password_digest, :null => false
 
+      t.string :activation_digest
+      t.boolean :activated, :default => false
+      t.datetime :activated_at
+
+      t.string :reset_digest
+      t.datetime :reset_sent_at
+
       t.timestamps
     end
     add_index :users, [:email], unique: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_01_085710) do
+ActiveRecord::Schema.define(version: 2020_09_08_150742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,13 +41,13 @@ ActiveRecord::Schema.define(version: 2022_02_01_085710) do
     t.string "last_name", limit: 255
     t.string "email", limit: 255, null: false
     t.string "password_digest", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "reset_digest"
-    t.datetime "reset_sent_at"
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 

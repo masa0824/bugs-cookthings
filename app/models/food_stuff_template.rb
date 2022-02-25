e47b151db3a6333364d980637ac8bc5cba4a504f
masa0824 <87467360+masa0824@users.noreply.github.com>
2022-02-25 13:51:15 +0900

    材料登録にバリデーターを導入

--- app/models/food_stuff_template.rb
+++ app/models/food_stuff_template.rb
@@ -1,3 +1,15 @@
 class FoodStuffTemplate < ApplicationRecord
     belongs_to :recipe_template
+
+    validates :food_stuff,
+        presence: true,
+        length: { maximum: 20 }
+    
+    validates :amount,
+        presence: true,
+        numericality: {greater_than_or_equal_to: 1}
+    
+    validates :mass,
+        presence: true,
+        length: { maximum: 20 }
 end
